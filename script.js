e8a0ee1f2d713d744a525f0400fc4737cab9204c
Utkir <utkirkurbanov2700@gmail.com>
2022-09-29 15:35:24 +0500

    JavaScript Variables

---
+++ script.js
@@ -0,0 +1,11 @@
+var message = "Hello Developers! 2";
+var sayHello = "Hello world!";
+var hello_message = "Annyonghaseyo!";
+var $ = "Dollar";
+var _ = "Underscore";
+
+// console.log(message);
+// console.log(sayHello);
+// console.log(hello_message);
+// console.log(_);
+console.log($);
